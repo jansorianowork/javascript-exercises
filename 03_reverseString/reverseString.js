736bcf2428ebaa557fff947256e3f2242893c935
jansoriano <jansoriano.work@gmail.com>
2022-12-20 01:04:39 +0800

    3 and 4 is completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    
 
+    return stringToReverse.split("").reverse().join().replaceAll(",","")
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayHandler,...arraytoRemove) {
+        // filter each item in arrayHandler based on item in array remove
+        // use spread operator to filter each value of array remove into array handler
+        return arrayHandler.filter( val => !arraytoRemove.includes(val) );
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
