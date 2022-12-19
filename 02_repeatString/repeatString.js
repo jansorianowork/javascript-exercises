4966da26b3ff0c52d27271d5a36d689c8dd0bf22
jansoriano <jansoriano.work@gmail.com>
2022-12-19 15:46:21 +0800

    done exercise 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,20 @@
-const repeatString = function() {
+const repeatString = function(stringVal,multiplier) {
+      var stringValue = ""
+      var stringholder = stringVal
+
+      if (multiplier === 0){
+        return ""
+      }
+
+      if (multiplier < 0){
+        return "ERROR"
+      }
+    for (let i = 0; i < multiplier; i++) {
+      stringValue = stringValue.concat(stringholder)
+      }
+      return stringValue
+
+
 
 };
 
