24f84702e71ca8a55a42cf9d10cf35b3b5829b13
jansoriano <jansoriano.work@gmail.com>
2022-12-20 18:50:17 +0800

    completed tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,30 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(valFarenheit) {
+  //compute using the formula for Farenheit to Celius
+  var convertedToFarenheit = (valFarenheit - 32) * .5556
+
+  //convert to only accept 1 decimal
+  var convertToFahrenheitToOneDec = convertedToFarenheit.toFixed(1)
+  //convert from string to number since tofixed returns string
+  var convertedToFarenheitParse = parseFloat(convertToFahrenheitToOneDec)
+
+  return convertedToFarenheitParse
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(valCelcius) {
+//compute using the formula for Celius to Farenheit
+  var convertedToCelcius = (valCelcius * 1.8) + 32
+
+
+  //convert to only accept 1 decimal
+  var convertToCelciusToOneDec = convertedToCelcius.toFixed(1)
+
+
+    //convert from string to number since tofixed returns string
+
+  var convertedToCelciusParse = parseFloat(convertToCelciusToOneDec)
+
+  return convertedToCelciusParse
+
 };
 
 // Do not edit below this line
