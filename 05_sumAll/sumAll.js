e0a646b675de4134f2d1df1ffa40c98cb687924c
jansoriano <jansoriano.work@gmail.com>
2022-12-20 13:57:46 +0800

    completed sumall and leapyears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    const biggerNum = firstNum > secondNum ? firstNum:secondNum
+    //handle errors for negative numbers
+    if (firstNum <= 0 || secondNum <=0){
+        return "ERROR"
+    }
+    //handle errors for non-number parameters
+    if (typeof biggerNum != 'number'){
+        return "ERROR"
+    }
+    //total value handler
+    var sumCounter = 0
+    for ( var i = 0; i <= biggerNum ;i++){
+        //loop through the biggest number
+        sumCounter += i
+        //add 1 every loop
+    }
 
+    return sumCounter
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function (valYear) {
 
+    const valYearHandler = valYear;
+    if (valYearHandler % 4 === 0 ){
+        if (valYearHandler % 400 ===0 && valYearHandler % 100 ===0){
+            //returns true if val year is divisible by 400 but not 100
+            return true 
+        }
+        if (valYearHandler % 100 ===0){
+            //returns false if val year is divisible by 4 but not 100
+            return false 
+        }
+        return true
+    }else{
+        //return false if not divisible by 4, 400, 100
+        return false
+    }
+    
 };
 
 // Do not edit below this line
