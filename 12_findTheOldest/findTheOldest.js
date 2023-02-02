fa34ddcaa29817a533e3099ffbab71d879481211
jansoriano <jansoriano.work@gmail.com>
2023-02-03 00:29:01 +0800

    findtheoldest,caesar

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(ArrayPeople) {
+    let name = ArrayPeople.map(item=>item.name)
+    let birthYear = ArrayPeople.map(item=>item.yearOfBirth)
+    let deathYear = ArrayPeople.map(item=>item.yearOfDeath)
 
+    let peopleAge=[]
+   for(let i=0;i<ArrayPeople.length;i++){
+        let ans = 0
+        if(deathYear[i] === '' || deathYear[i] === 0 || deathYear[i]===undefined){
+            deathYear[i] = new Date().getFullYear();
+        }
+ 
+        ans = deathYear[i] - birthYear[i]
+
+        const personAge = {name:name[i],age:ans}
+        peopleAge.push(personAge)
+   }
+
+
+   let checkOldestAge = peopleAge.reduce((prev, curr)=>(curr.age > prev.age ? curr : prev))
+
+
+
+   return checkOldestAge
+ 
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,24 @@
-const caesar = function() {
+const caesar = function(message, shift) {
+    if (shift < 0) {
+        shift = 26 + (shift % 26);
+      }
+      return message
+        .split("") //splits it into an array
+        .map(message => {
+             //does the following to each element in the array
+          normalStr = String.fromCharCode(message.charCodeAt())
+          console.log(normalStr)
+          prePoint = message.charCodeAt() //gets the charcode of element  
+          //if/else checks to see if upper or lower case
+          if (prePoint >= 65 && prePoint <= 90) { //upper case
+            return String.fromCharCode(((prePoint - 65 + shift) % 26) + 65);
+          } else if (prePoint >= 97 && prePoint <= 122) { //lower case
+            return String.fromCharCode(((prePoint - 97 + shift) % 26) + 97)
+          } else {
+            return normalStr;
+          }
+        })
+        .join("");
 
 };
 
